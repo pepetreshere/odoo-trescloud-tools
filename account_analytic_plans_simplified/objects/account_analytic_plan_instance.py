5613aab4ef02d6100baea5685244dac4d9f65dae
Andres Calle <andres@andres-calle>
2014-07-26 12:27:36 -0500

    IN PROGRESS - correcion bug redondeo en centros de costos distribuidos

--- account_analytic_plans_simplified/objects/account_analytic_plan_instance.py
+++ account_analytic_plans_simplified/objects/account_analytic_plan_instance.py
@@ -21,7 +21,6 @@
 from openerp.osv import fields, osv
 from openerp import netsvc
 from openerp.tools.translate import _
-import unittest #para comparar floats
 
 
 class account_analytic_plan_instance(osv.osv):
